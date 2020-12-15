6679b5ae8b80f880286299f4ec31b9f8aa2535f0
pnxenopoulos <pnxenopoulos@gmail.com>
2020-12-15 11:31:26 -0500

    added basic calibration test

--- templates.py
+++ templates.py
@@ -4,8 +4,10 @@ TEMPLATES = {}
 
 # Baseball
 TEMPLATES["baseball"] = {}
-TEMPLATES["baseball"]["home_plate"] = np.array([0,0,0])
-TEMPLATES["baseball"]["pitchers_mound"] = np.array([0, 18.39, 0])
-TEMPLATES["baseball"]["first_base"] = np.array([18.39, 18.39, 0])
-TEMPLATES["baseball"]["second_base"] = np.array([0, 36.78, 0])
-TEMPLATES["baseball"]["third_base"] = np.array([-18.39, 18.39, 0])
+TEMPLATES["baseball"]["home_plate"] = np.array([0, 0, 0], dtype=np.float32)
+TEMPLATES["baseball"]["left_batters"] = np.array([-1, 0, 0], dtype=np.float32)
+TEMPLATES["baseball"]["right_batters"] = np.array([1, 0, 0], dtype=np.float32)
+TEMPLATES["baseball"]["pitchers_mound"] = np.array([0, 18.39, 0], dtype=np.float32)
+TEMPLATES["baseball"]["first_base"] = np.array([18.39, 18.39, 0], dtype=np.float32)
+TEMPLATES["baseball"]["second_base"] = np.array([0, 36.78, 0], dtype=np.float32)
+TEMPLATES["baseball"]["third_base"] = np.array([-18.39, 18.39, 0], dtype=np.float32)
