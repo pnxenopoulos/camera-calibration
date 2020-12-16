8b9791c05c0e55e280b938cb559667988d8052df
pnxenopoulos <pnxenopoulos@gmail.com>
2020-12-16 14:59:33 -0500

    added pitcher_mound_left to template

--- templates.py
+++ templates.py
@@ -2,17 +2,18 @@ import numpy as np
 
 TEMPLATES = {}
 
-# Baseball
+# Baseball, in meters
 TEMPLATES["baseball"] = {}
 TEMPLATES["baseball"]["home_plate"] = np.array([0, 0, 0], dtype=np.float32)
 TEMPLATES["baseball"]["left_batters"] = np.array([-1, 0, 0], dtype=np.float32)
 TEMPLATES["baseball"]["right_batters"] = np.array([1, 0, 0], dtype=np.float32)
-TEMPLATES["baseball"]["pitchers_mound"] = np.array([0, 18.39, 0], dtype=np.float32)  # or 0.254?
+TEMPLATES["baseball"]["pitchers_mound_center"] = np.array([0, 18.39, 0], dtype=np.float32)  # or 0.254?
+TEMPLATES["baseball"]["pitchers_mound_left"] = np.array([-0.4572, 18.39, 0], dtype=np.float32)
 TEMPLATES["baseball"]["first_base"] = np.array([18.39, 18.39, 0], dtype=np.float32)
 TEMPLATES["baseball"]["second_base"] = np.array([0, 36.78, 0], dtype=np.float32)
 TEMPLATES["baseball"]["third_base"] = np.array([-18.39, 18.39, 0], dtype=np.float32)
 
-# Basketball
+# Basketball. in feet
 TEMPLATES["basketball"] = {}
 TEMPLATES["basketball"]["left_basket_base"] = np.array([0, 0, 0], dtype=np.float32)
 TEMPLATES["basketball"]["left_basket_base_left"] = np.array([0,8,0], dtype=np.float32)
